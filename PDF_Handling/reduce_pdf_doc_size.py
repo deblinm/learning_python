fd2e798c0aaf6064244a59acf6afa4bec1ac5927
deblinm <deblinm18@gmail.com>
2026-01-01 15:21:32 -0500

    first commit

--- PDF_Handling/reduce_pdf_doc_size.py
+++ PDF_Handling/reduce_pdf_doc_size.py
@@ -2,17 +2,12 @@ import pypdf
 
 input_path =input("Please enter a pdf file with complete path: ")
 modified_path=input_path.replace('/','\\')
-reader=pypdf.PdfReader(modified_path)
-writer=pypdf.PdfWriter()
-
-for page in reader.pages:
-    writer.add_page(page)
+writer=pypdf.PdfWriter(clone_from=modified_path)
 
 for page in writer.pages:
     page.compress_content_streams(level=9)
 
-with open(f"{modified_path}_compressed", "wb") as output_file:
-    writer.write(output_file)
+writer.write(f"{modified_path}_compressed.pdf")
 
 
 
