a2a8f97aa1ab217481e6d4fe7e4030d917200863
deblinm <deblinm18@gmail.com>
2026-02-16 08:19:40 -0500

    main modified to support new updated structure

--- config_driven_data_validator/main.py
+++ config_driven_data_validator/main.py
@@ -2,28 +2,38 @@ import json
 import pandas as pd
 import validator.engine as e
 
-
-data = {
+def load_data():
+    data = {
     "customer_id": [1, 2, 3, 4, 5],
     "age": [25, None, 40, 17, 65],
     "salary": [50000, 60000, None, 30000, -120000],
     "country": ["US", "IND", "CA", "MX", None]
 }
 
-df = pd.DataFrame(data)
-
-with open("config/sample_config.json","r") as conf:
-    config = json.load(conf)
-
-output = e.run_validation(df, config)
-
-print("Summary:")
-print(output["summary"])
-
-print("\nDetailed Results:")
-for r in output["results"]:
-    print(r)
+    df = pd.DataFrame(data)
+    return df
 
 
-# Production enforcement
-e.enforced_pipeline(output["summary"], config)
+def load_config(path):
+    with open (path,"r") as f:
+        return json.load(f)
+
+
+def main():
+    config_data = load_config("config/sample_config.json")
+    df = load_data()
+    output = e.run_validation(df, config_data)
+
+    print("Summary:")
+    print(output["summary"])
+
+    print("\nDetailed Results:")
+    for r in output["results"]:
+        print(r)
+
+    # Production enforcement
+    e.enforced_pipeline(output["summary"], config_data)
+
+
+if __name__ == "__main__":
+    main()
